3706b16510aad432e3fc8186ce7f6ef4762c915e
Pedro Haick <pedrohaickzz@gmail.com>
2022-05-17 22:16:03 -0300

    Removing node_modules from repo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const app = express();
+const cors = require('cors');
 const connectToDB = require("./db");
 const Campground = require("./models/campground");
+
+const app = express();
 const port = 3000;
+
+app.use(cors())
 
 app.listen(port, () => {
   connectToDB();
